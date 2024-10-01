3c59fd44cdb70d22930789959aab69197affe5db
maite <maite.sanchez@gm2dev.com>
2024-10-01 12:36:13 -0300

    feat(random-number): full activity

---
+++ random-number/Asynchronous/fetch/fetch.js
@@ -0,0 +1,47 @@
+/* Primer practica con fetch: 
+
+Mostrar en forma de tabla todos los usuarios de la siguiente API: Users API. La tabla tendrá que contener un filtro que permita filtrar por rol a los usuarios.
+
+Cada row de la tabla deberá contener lo siguiente:
+
+Id
+Avatar
+Nombre
+Mail */
+
+function getApi (){
+    let loading = document.getElementById('loading');
+
+    fetch('https://643ecf8ec72fda4a0b01bc66.mockapi.io/api/v1/users')
+    .then((response) => response.json())
+    .then((data) => updateTable(data))
+    .catch((error) => console.error('Error loading users', error))
+    .finally(() => loading.classList.remove('loading'))
+}
+
+window.onload = getApi();
+
+function updateTable (users){
+    let usersTable = document.querySelector(".users-table");
+    usersTable.innerHTML = "";
+
+    users.forEach(user => {
+        let row = document.createElement('tr');
+
+        let idColumn = document.createElement('td');
+        idColumn.textContent = user.id;
+        row.appendChild(idColumn);
+
+        let nameColumn = document.createElement('td');
+        nameColumn.textContent = user.name;
+        row.appendChild(nameColumn);
+
+        let mailColumn = document.createElement('td');
+        mailColumn.textContent = user.email;
+        row.appendChild(mailColumn);
+
+        usersTable.appendChild(row);
+    });
+
+    usersTable.classList.remove('.hidden');
+}
